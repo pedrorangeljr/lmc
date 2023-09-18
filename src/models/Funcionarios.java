da27951c8fc8c7ed3c2b39b58069ab1ce1aa4313
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-09-18 11:36:50 -0300

    Model Funcionario

---
+++ src/models/Funcionarios.java
@@ -0,0 +1,120 @@
+package models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Funcionarios implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	private Long idFuncionario;
+	private String nome;
+	private String telefone;
+	private Date dataNascimento;
+	private String email;
+	private String rg;
+	private String sexo;
+	private Date dataCadastro;
+	private String cpf;
+	private String cep;
+	private String logradouro;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	
+	
+	public Long getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(Long idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
+	
+}
